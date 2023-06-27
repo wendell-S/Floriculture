1a4141eb8b38219b78faba87b2a341f835455762
wendell <123216078+wendell-S@users.noreply.github.com>
2023-06-27 19:31:50 -0300

    V0.2.2
    correção de erros
    coneçao com ccs

--- src/main/resources/static/js/PrincipalTela.js
+++ src/main/resources/static/js/PrincipalTela.js
@@ -5,6 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
  window.location.href = 'TelaMensagem.html';
  });
 });
-
-var nome = window.prompt('ola, como se chamas?')
-window.alert('prazer em te conhecer ' + nome,  ' fique avontade para explorar o site em desenvolvimento')
